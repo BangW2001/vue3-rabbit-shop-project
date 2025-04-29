8cc0b1728164957fc4ee9f25a2cc9155e4d6e155
BangW2001 <bangwu2001@gmail.com>
2025-04-29 16:26:54 +0800

    vue3-watch

--- my-file/code/01-vue3入门/vue3-project/src/App.vue
+++ my-file/code/01-vue3入门/vue3-project/src/App.vue
@@ -1,47 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { ref,watch } from 'vue'
+  const people = ref({
+    name:"zs",
+    age:27
+  })
+  //第一个参数为需要监听的特定属性
+  watch(
+    ()=>people.value.age,
+    (newValue,oldValue)=>console.log(`age的值发生了变化,原来的值为${oldValue},现在的值为:${newValue}`)
+  )
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>姓名：{{ people.name }}</div><br>
+  <div>年龄：{{ people.age }}</div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
