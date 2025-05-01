0eea734eb3628a751e7ebad2bd1bd8587f63813e
Bangwu2001 <1985439923@qq.com>
2025-05-01 22:51:46 +0800

    安装sass并按照官方文档配置elementPlus主题色

--- my-file/code/rabbit-web/vite.config.js
+++ my-file/code/rabbit-web/vite.config.js
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -7,7 +7,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,12 +16,23 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver({
+              importStyle: "sass",
+              directives: true,
+              version: "2.1.5",
+            })],
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/element/index.scss" as *;`,
+      },
     },
   },
 })
