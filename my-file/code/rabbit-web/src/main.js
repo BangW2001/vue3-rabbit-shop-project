5c91fc257644dac92396fad7b33f42dd3cf103bf
Bangwu2001 <1985439923@qq.com>
2025-05-01 23:47:33 +0800

    axios基础封装

--- my-file/code/rabbit-web/src/main.js
+++ my-file/code/rabbit-web/src/main.js
@@ -3,7 +3,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import getCategory from './apis/testApi'
 
+getCategory().then((res)=>{
+  console.log(res)
+})
 const app = createApp(App)
 
 app.use(createPinia())
