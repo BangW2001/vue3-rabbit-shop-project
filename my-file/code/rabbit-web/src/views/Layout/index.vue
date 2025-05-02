be830e9021a33d32ec0112d985d26d821743766f
Bangwu2001 <1985439923@qq.com>
2025-05-02 16:10:00 +0800

    layout页面静态结构搭建

--- my-file/code/rabbit-web/src/views/Layout/index.vue
+++ my-file/code/rabbit-web/src/views/Layout/index.vue
@@ -1,4 +1,13 @@
 <template>
-  <div>这是首页</div>
-  <RouterView/>
+  <div>
+    <NavLayout></NavLayout>
+    <HeaderLayout></HeaderLayout>
+    <RouterView/>
+    <FooterLayout></FooterLayout>
+  </div>
 </template>
+<script setup>
+import NavLayout from '@/views/Layout/components/NavLayout.vue';
+import HeaderLayout from '@/views/Layout/components/HeaderLayout.vue';
+import FooterLayout from '@/views/Layout/components/FooterLayout.vue';
+</script>
