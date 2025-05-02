932eff54c46e11ecaa4b72246ec9a0d0e7d6450d
Bangwu2001 <1985439923@qq.com>
2025-05-02 15:48:19 +0800

    配置scss自动导入

--- my-file/code/rabbit-web/src/App.vue
+++ my-file/code/rabbit-web/src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <router-view></router-view>
+  <div class="test">
+    scss文件自动导入
+  </div>
 </template>
 
 <script lang="ts" setup>
 
 </script>
+<style scoped lang="scss">
+.test{
+  color: $warnColor;
+}
+</style>
