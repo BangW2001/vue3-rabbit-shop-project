6daaca46ed82b1fa594c653c367d991a5f2473a3
BangW2001 <bangwu2001@gmail.com>
2025-05-04 10:22:35 +0800

    home页新鲜好物和人气推荐页面

--- my-file/code/rabbit-web/src/apis/home.js
+++ my-file/code/rabbit-web/src/apis/home.js
@@ -11,3 +11,26 @@ function getBannerAPI (){
 }
 
 export default getBannerAPI
+
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = () => {
+  return httpInstance({
+    url:'/home/new'
+  })
+}
+
+/**
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
+export const getHotAPI = () => {
+  return  httpInstance({
+    url:'home/hot'
+  })
+}
--- my-file/code/rabbit-web/src/views/Home/inde.vue
+++ my-file/code/rabbit-web/src/views/Home/inde.vue
@@ -4,7 +4,6 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import HomeProduct from './components/HomeProduct.vue'
-import HomePannel from './components/HomePannel.vue'
 </script>
 
 <template>
@@ -15,7 +14,4 @@ import HomePannel from './components/HomePannel.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
-  <HomePannel title="新鲜好物" sub-title="新鲜好物 好多商品">
-    <template v-slot:main><div>新鲜好物的插槽</div></template>
-  </HomePannel>
 </template>
